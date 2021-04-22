41f152cfb0597ab8aceaff7f45ef5a021439e974
ejunior95 <e.junior95@gmail.com>
2021-04-21 23:20:14 -0300

    [UPDATE]->Criação das páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './pages/Home';
+import Detalhes from './pages/Detalhes';
 
 function App() {
   return (
-    <div>
-     <h1>Teste</h1>
-    </div>
+    <Router>
+        <Switch>
+          <Route path="/details">
+            <Detalhes />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      
+    </Router>
   );
 }
 
---
+++ src/pages/Detalhes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Detalhes: React.FC = () => {
+  return(
+      <Container>
+          <h1>Detalhes</h1>
+      </Container>
+  );
+}
+
+export default Detalhes;

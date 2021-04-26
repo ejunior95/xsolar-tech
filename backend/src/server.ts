af6a663dfad9b654014cda00a9f9ee1bb317b6cf
ejunior95 <e.junior95@gmail.com>
2021-04-25 23:12:37 -0300

    [UPDATE]-> Iniciando criação da rota de cadastro de clientes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,6 +10,35 @@ server.get('/', (request, response) => {
 // Rotas autenticadas
 
 server.post('/login', (request, response) => {
+    const {
+        login,
+        senha
+    } = request.body
+
+    if (senha !== "1234" || login !== "admin") {
+        return response.status(401).json({
+            message: "Login ou senha inválida!"
+        })
+    }
+    return response.status(200).json({
+        message: "Acesso autorizado!",
+        token: "Berer 102030dsadsalkkoekopqw913892132145dsa564d56sa421921"
+    })
+})
+
+// Rotas do CRUD
+
+server.post('/clientes', (request, response) => {
+    const {
+        nome,
+        cpf,
+        telefone,
+        email,
+        enderecoPrincipal,
+        enderecosSecundarios
+    } = request.body
+
+    
    
 })
 

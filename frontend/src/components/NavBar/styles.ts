e2648a91a86d886ddbecefd24b2fd44eb37eea10
ejunior95 <e.junior95@gmail.com>
2021-05-01 02:46:40 -0300

    [UPDATE]-> Finalizando estilização da NavBar

--- frontend/src/components/NavBar/styles.ts
+++ frontend/src/components/NavBar/styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
   left: 0;
   width: 100%;
   height: 12vh;
-  background-color: #133ABA;
+  background-color: #0C29F2;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -31,8 +31,17 @@ export const Container = styled.div`
   .container-itens-menu ul li {
     font-size: 1rem;
     list-style: none;
-    padding: 5px;
+    padding: 8px;
     cursor: pointer;
+    border-radius: 5px;
+    transition: .2s ease-in-out;
+    border: 1px solid #0C29F2;
+  }
+
+  .container-itens-menu ul li:hover {
+    background-color: #0D69FC;
+    border: 1px solid #ffffff;
+    transition: .2s ease-in-out;
   }
 
 `;

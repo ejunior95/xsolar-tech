8abd403f1d9bec4dda75be8df53bb47447636762
ejunior95 <e.junior95@gmail.com>
2021-05-02 19:53:33 -0300

    [UPDATE]-> Criação dos componentes de gráficos

---
+++ frontend/src/components/BarChart/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './styles';
+
+const BarChart: React.FC = () => {
+  return <div />;
+}
+
+export default BarChart;
---
+++ frontend/src/components/PieChart/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './styles';
+
+const PieChart: React.FC = () => {
+  return <div />;
+}
+
+export default PieChart;
